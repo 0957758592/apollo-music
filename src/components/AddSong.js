2e9889b511ae320811dddbbe86ac9e6414aeb9ec
Roman Danilov <r.danylov@gmail.com>
2020-05-25 19:01:47 +0300

    popup dialog added

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { TextField, InputAdornment, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import { Link, AddBoxOutlined } from '@material-ui/icons';
+
 
 function AddSong() {
+
+    const [dialog, setDialog] = React.useState(false)
+    
+
+    function handleClose() {
+        setDialog(false)
+    }
+
     return (
-        <div >
-            AddSong
+        <div>
+            <Dialog
+                open={dialog}
+                onClose={handleClose}
+            >
+                <DialogTitle>
+                    Edit Song
+                </DialogTitle>
+                <DialogContent>
+                    <img src="https://ww1.prweb.com/prfiles/2014/04/10/11752526/gI_134971_best-image-web-hosting.png" alt="Song tumbnail"/> 
+                </DialogContent>
+                <TextField
+                    margin="dense"
+                    name='title'
+                    label="Title"
+                    fullWidth
+                />
+                <TextField
+                    margin="dense"
+                    name='artist'
+                    label="Artist"
+                    fullWidth
+                />
+                <TextField
+                    margin="dense"
+                    name='thumbnail'
+                    label="Thumbnail"
+                    fullWidth
+                />
+                <DialogActions>
+                    <Button onClick={handleClose} color="secondary">
+                        Cancel
+                    </Button>
+                    <Button variant="outlined" onClick={handleClose} color="primary">
+                        Add Song
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <TextField
+                placeholder="Add YouTube or SoundCloud Url"
+                fullWidth
+                type='url'
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <Link/>
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button
+                variant='contained'
+                color="primary"
+                endIcon={<AddBoxOutlined />}
+                onClick={() => setDialog(true)}
+            >
+                Add
+            </Button>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import theme from './components/utils/theme';
 
 
-
-
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <React.StrictMode>
       <CssBaseline />
       <App />
-    </React.StrictMode>
   </MuiThemeProvider>,
   document.getElementById('root')
 );
