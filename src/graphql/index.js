f032920aefb03970bdac5d31b64fce212b4e38a7
Roman Danilov <r.danylov@gmail.com>
2020-05-31 01:04:15 +0300

    refactor client, add cache

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,21 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { InMemoryCache } from "apollo-cache-inmemory"
 
-const client = new ApolloClient({
-    uri: "https://apollo-music-a.herokuapp.com/v1/graphql"
+const client  = new ApolloClient({
+    link: new WebSocketLink({
+        uri: 'wss://apollo-music-a.herokuapp.com/v1/graphql',
+        options: {
+            reconnect: true
+        }
+    }),
+    cache: new InMemoryCache()
 })
+
+// import ApolloClient from 'apollo-boost'
+
+// const client = new ApolloClient({
+//     uri: "https://apollo-music-a.herokuapp.com/v1/graphql"
+// })
 
 export default client;
