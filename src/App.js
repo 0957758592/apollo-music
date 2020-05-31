6f16ffac5036943c9de32e627b6d970b9bbad6a6
Roman Danilov <r.danylov@gmail.com>
2020-05-31 03:58:35 +0300

    add song reducer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import AddSong from './components/AddSong'
 import SongList from './components/SongList'
 import SongPlayer from './components/SongPlayer'
 import { Grid, useMediaQuery, Hidden } from "@material-ui/core"
+import songReducer from './store/reducer'
+
+export const SongContext = React.createContext({
+  song: {
+    id: 'cdefc2ca-fd9c-415f-b84c-7742a34f8785',
+    title: 'Unforgivable(First State Remix Edit)',
+    artist: "Armin",
+    thumbnail: 'https://img.youtube.com/vi/FPPchfgTCZ0/0.jpg',
+    url: 'https://www.youtube.com/watch?v=FPPchfgTCZ0&list=LL3xmAZGZZ04QU9qc7fQqvvg&index=7&t=0s',
+    duration: 400
+  },
+  isPlaying: false
+})
 
 function App() {
-
+  const initialSongState = React.useContext(SongContext)
+  const [state, dispatch] = React.useReducer(songReducer, initialSongState)
   const greaterThanSM = useMediaQuery(theme => theme.breakpoints.up('sm'))
   const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'))
 
   return (
-    <div >
+    <SongContext.Provider value={{state, dispatch}} >
       <Hidden only='xs'>
         <Header />
       </Hidden>
@@ -37,7 +51,7 @@ function App() {
         </Grid>
       </Grid>
 
-    </div>
+    </SongContext.Provider>
   );
 }
 
